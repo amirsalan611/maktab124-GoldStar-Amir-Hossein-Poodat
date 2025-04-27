696699438cef396051a3628f5a3518fa94523828
arsalan611 <amir.arsalan.poudat@gmail.com>
2025-04-27 22:39:47 +0330

    feat: update getProducts component

--- src/services/auth/getProducts/GetProducts.ts
+++ src/services/auth/getProducts/GetProducts.ts
@@ -5,7 +5,9 @@ interface GetProductsParams {
   id?: string;
   type?: "category" | "subcategory";
   page: number;
-  discount?:string
+  discount?: string;
+  priceOneM?: boolean;
+  price500?: boolean;
 }
 
 export const getProducts = async ({
@@ -13,11 +15,19 @@ export const getProducts = async ({
   type,
   page,
   discount,
+  priceOneM,
+  price500,
 }: GetProductsParams) => {
   let url = `${BASE_URL}/api/products?limit=10&page=${page}`;
-  
+
   if (type && id) {
     url += `&${type}=${id}`;
+  }
+  if (priceOneM) {
+    url += `&price[lt]=1000000`;
+  }
+  if (price500) {
+    url += `&price[lt]=500000`;
   }
   if (discount) {
     url += `&discount[gt]=0`;
