e7484116904b5895af451b445fb277f3e176b5e2
arsalan611 <amir.arsalan.poudat@gmail.com>
2025-04-29 20:58:25 +0330

    feat: add js-cookie and redux-persist-cookie-storage dependencies with type definitions

---
+++ src/types/redux-persist-cookie-storage.d.ts
@@ -0,0 +1,11 @@
+declare module "redux-persist-cookie-storage" {
+  export class CookieStorage {
+    constructor(
+      cookies: any,
+      options?: { expiration?: { default?: number }; secure?: boolean }
+    );
+    getItem(key: string): Promise<string | null>;
+    setItem(key: string, value: string): Promise<void>;
+    removeItem(key: string): Promise<void>;
+  }
+}
