29ae118552dfbeb62b1f3e55355d3a25618dcc4a
arsalan611 <amir.arsalan.poudat@gmail.com>
2025-05-05 14:13:42 +0330

    feat: add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='flex justify-center items-center h-[500px]'>
+      صفحه مورد نطر یافت نشد
+    </div>
+  )
+}
